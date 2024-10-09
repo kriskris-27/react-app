449c6d4c9ac7290b34c681d14b2c57a454cdbab5
Krishnakumar <krishnakumars0101@gmail.com>
2024-10-09 19:24:14 +0530

    Added new example

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Message from './message'
+import Message from './Message'
 
 function App() {
   return <div><Message/></div>
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,9 @@
 function Message() {
-    return <h1>HELLO World</h1>
+
+    const name='';
+    if(name)
+    return <h1>HELLO {name}</h1>
+    return <h1>HELLO WORLD</h1>
 }
 
 export default Message;
