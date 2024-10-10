74c6b2e9ea751f622b9de1b9d9586f2c0411bcf7
Krishnakumar <krishnakumars0101@gmail.com>
2024-10-10 22:11:26 +0530

    new button example

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,9 +1,26 @@
 function Message() {
-
-    const name='';
-    if(name)
-    return <h1>HELLO {name}</h1>
-    return <h1>HELLO WORLD</h1>
-}
-
-export default Message;
+    const name = 'KRIS';
+    if (name) {
+      return <h1>HELLO {name}</h1>;
+    }
+    return <h1>HELLO WORLD</h1>;
+  }
+  
+  function Button() {
+    return <button>Click Me!</button>;
+  }
+  
+  function Button2() {
+    return <button>Don't Click Me!</button>;
+  }
+  
+  export default function App() {
+    return (
+      <div>
+        <Button />
+        <Button2 />
+        <Button />
+      </div>
+    );
+  }
+  
